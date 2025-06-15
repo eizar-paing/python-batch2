b84fdfe0947ac2fab1607791251255a39c7d4338
eizar-paing <eizarpaing.sg@gmail.com>
2025-06-15 19:38:05 +0900

    file open

--- files/open_file.py
+++ files/open_file.py
@@ -1,2 +1,21 @@
-f = open("demo-file.txt", "rt")
-print(f.read())
+import os
+
+if os.path.exists("file-dir/test/demo-file.txt"):
+  f = open("file-dir/test/demo-file.txt", "rt")
+  print(f.read())
+  f.close()
+else:
+  print("demo-file.txt is not exist to read.")
+
+with open("file-dir/test/demo-file.txt", "a") as f:
+  f.write(" This is new line.")
+
+with open("file-dir/test/demo-file.txt") as f:
+  print(f.readline())
+
+if os.path.exists("demo-file.txt"):
+  os.remove("demo-file.txt")
+else:
+  print("demo-file.txt is not exist to remove.")
+
+os.rmdir("test")
---
+++ user-input/menu_input.py
@@ -0,0 +1,7 @@
+1. start
+2. exit
+
+1. addition
+2. percentage
+3. subtraction
+
